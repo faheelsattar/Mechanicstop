83731d887dcea9844bc27b20cdcdf0da05d947bb
faheelsattar <faheelsattar@yahoo.com>
2019-12-08 22:07:32 +0500

    Add files via upload
    headers updated

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app= express();
 const mysql = require('mysql');
 const bodyparser= require('body-parser')
 const dotenv= require('dotenv');
+const authRoute= require('./routes/auth');
+const userRoutes= require('./routes/user');
+const mechanicRoutes=require('./routes/mechanic')
+const requestRoutes= require('./routes/requests')
+
 
 dotenv.config();
 const connection = mysql.createConnection({
@@ -15,16 +20,23 @@ connection.connect(err =>{
     if(err){
         return err;
     }
-});
+}); 
 console.log(connection)
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
-const authRoute= require('./routes/auth');
-app.use('/',authRoute)
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, auth-token");
+    next();
+  });
+app.use('/auth',authRoute)
+app.use('/user',userRoutes)
+app.use('/mechanic',mechanicRoutes)
+app.use('/requests',requestRoutes)
 app.get('/',(req,res)=>{
     res.send('Home');
 })
 
-app.listen(3000, ()=>{
+app.listen(4000, ()=>{
     console.log("Server up and running")
 })

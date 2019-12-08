32cccb467ed0354e7bfbe818aad1eac4da8b35a6
faheelsattar <faheelsattar@yahoo.com>
2019-12-08 22:06:39 +0500

    Add files via upload
    Jwts/ users extraction

--- middlewares/tokenauth.js
+++ middlewares/tokenauth.js
@@ -2,10 +2,13 @@ const jwt=require('jsonwebtoken');
 
 const tokenVerification=(req,res,next)=>{
     const token = req.header('auth-token');
+    console.log(token)
     if(!token)
     return res.status(401).send('Access denied');
     try{
     var checker=jwt.verify(token,process.env.Secret_Key);
+    console.log(checker)
+    res.locals.user=checker.id
     next();
     }catch(err){
         res.status(400).send('Invalid token');
